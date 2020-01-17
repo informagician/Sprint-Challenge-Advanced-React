3ebb30a0afb3be4dc3a4cf32b192c60637ba94a1
Milo Rastgoo <hello@informagician.com>
2020-01-17 11:56:45 -0500

    loading from API

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  state = {
+    playerData: []
+  };
+  
+
+  componentDidMount() {
+    axios
+    .get('http://localhost:5000/api/players')
+    .then(res => {
+      this.setState({playerData: [...res.data]})
+      console.log(this.state.playerData)
+    })
+    .catch(err => console.log(err))
+  }
+
+  render() {
+    console.log(this.state.playerData)
+    return (
+      <div className="App">
+        {this.state.playerData.map(item => (
+          <div key={item.id}>
+            <span>{item.name}</span>
+            <span>{item.country}</span>
+            <span>{item.searches}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
